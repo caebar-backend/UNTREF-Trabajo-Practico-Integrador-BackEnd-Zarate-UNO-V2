c9490909bce5d0cd860b6952a008d1e6736b2dda
[caebar] <[caebar.cursos.programacion@gmail.com]>
2025-06-30 17:59:57 -0300

    Commit Ocho - Creacion y escritura del archivo errorController.js en la carpeta controllers

---
+++ controllers/errorController.js
@@ -0,0 +1,9 @@
+
+exports.rutaNoEncontradaInexistente = (req, res) => {
+    const { url } = req
+    res.status(404).json({
+    mensaje: [' ------------------- ',' ------------------------- ','Ruta no encontrada o inexistente','Dirección inexistente en éste servidor!!',' ------------------- ',' ------------------------- ']
+  })
+  console.log('\x1b[101m Ruta no encontrada o inexistente... Dirección inexistente en éste servidor!! -> \x1b[0m', url)
+}
+
