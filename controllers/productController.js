f24be4655c3b9d460c75517f937bedd0e920f019
[caebar] <[caebar.cursos.programacion@gmail.com]>
2025-06-30 18:15:22 -0300

    Commit Once - Descripción y armado del punto > 2.1 Obtener un producto con un código inexistente <

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,3 +33,44 @@ exports.todasLasPrendas = (req, res) => {
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // 2. Obtener un producto por su código
+// 2.1 Obtener un producto con un código inexistente (debería dar 404)
+
+exports.prendasPorCodigo = (req, res) => {
+    const codigoSP = req.params.codigo
+    const codigoParseado = parseInt(codigoSP)
+    if(isNaN(codigoParseado)){
+            console.log('\x1b[31m El código ingresado no es numérico! \x1b[0m')
+            return res.status(400).json({ mensaje: 'El código ingresado no es numérico!'})
+        }
+    PRENDASENDB.find({ codigo: codigoParseado})
+    .then((prenda) => {
+        if(!prenda || prenda.length === 0){
+            console.log('\x1b[31m No existe un producto con ese código! \x1b[0m')
+            return res.status(404).json({ mensaje: 'No existe un producto con ese código!'})
+        }
+        // --- MAPEO + Console.table para mostrar x consola el producto en la Base de datos ----
+        
+        const existePrenda = prenda.map((prendita) => {
+            return {
+                codigo: prendita.codigo,
+                nombre: prendita.nombre,
+                precio: prendita.precio,
+                categoria: prendita.categoria
+            }
+        })
+    
+        console.log(`\x1b[103m Producto encontrado con el código -> ${codigoParseado} <- : \x1b[0m`)
+        console.table(existePrenda)
+        console.log('\x1b[103m -------------------------------------------------------------------- \x1b[0m')
+        //---------------------------
+        return res.status(200).json(prenda)
+    })
+    .catch((ERROR) => {
+        console.log('\x1b[31m No se pudo acceder a la prenda con ese código! -> \x1b[0m', ERROR)
+        return res.status(500).json({ mensaje: 'No se pudo acceder a la prenda con ese código!', error: ERROR })
+    })
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
